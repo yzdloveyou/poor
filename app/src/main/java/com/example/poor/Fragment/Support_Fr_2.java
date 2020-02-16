af9ccdc0c62d2f375e9b3d0227ddf7a9c659987c
tongsuh-temp <tongsuh98@gamil.com>
2020-02-16 21:28:09 +0800

    新增frag2，wangge2，redbg.png,reg.png 修改fragment2

--- app/src/main/java/com/example/poor/Fragment/Support_Fr_2.java
+++ app/src/main/java/com/example/poor/Fragment/Support_Fr_2.java
@@ -7,15 +7,19 @@ import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 
+import com.example.poor.R;
+
 import me.yokeyword.fragmentation.SupportFragment;
 
 public class Support_Fr_2 extends SupportFragment  {
+    GridView gridView;
 
     public static Support_Fr_2 newInstance() {
         Bundle args = new Bundle();
@@ -27,8 +31,12 @@ public class Support_Fr_2 extends SupportFragment  {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return null;
+       View view2=inflater.inflate(R.layout.frag2,null);
+
+        return view2;
     }
+
+
 }
 
 
